5fce74cbc373d3bfa79557f77f0e5c35ee73061f
myarmolinsky <yarmolinskymatthew@gmail.com>
2023-02-09 19:21:31 -0500

    Added bullet

--- Scripts/PlayerBullet.cs
+++ Scripts/PlayerBullet.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
+    public float speed = 7.5f;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // destroy the gameObject that this script is attached to
+        Destroy(gameObject);
     }
 }
